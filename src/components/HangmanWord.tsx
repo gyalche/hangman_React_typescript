44e01e0f247f84566549f94834512bccbc3d5acc
Dawa <sherpadawa01@outlook.com>
2022-11-15 22:00:01 +0545

    working on hangman words

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
 const HangmanWord = () => {
+  let guessLetter = ['t'];
+  const word = 'dawa';
   return (
     <div
       style={{
         display: 'flex',
+        gap: '.25rem',
+        fontSize: '4rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
       }}>
-      HangmanWord
+      {word.split('').map((letter, index) => (
+        <span style={{ borderBottom: '.1em solid black' }}>
+          <span
+            style={{
+              visibility: guessLetter.includes(letter) ? 'visible' : 'hidden',
+            }}>
+            {index}
+          </span>
+        </span>
+      ))}
     </div>
   );
 };
